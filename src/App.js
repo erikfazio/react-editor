66638b734aad7fe1f8e31cb29d617c46708a5e3f
erikfazio <fazioerik@gmail.com>
2021-06-21 17:30:24 +0200

    Make some tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import EditorJs from "react-editor-js";
+
+import { EDITOR_JS_TOOLS } from "./constants";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EditorJs
+        tools={EDITOR_JS_TOOLS}
+        data={{
+          time: 1556098174501,
+          blocks: [
+            {
+              type: "paragraph",
+              data: {
+                text:
+                  "We have been working on this project more than three years. Several large media projects help us to test and debug the Editor, to make it's core more stable. At the same time we significantly improved the API. Now, it can be used to create any plugin for any task. Hope you enjoy. 😏"
+              }
+            },
+          ],
+          version: "2.12.4"
+        }}
+      />
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,19 @@
+import Table from "@editorjs/table";
+import List from "@editorjs/list";
+import LinkTool from "@editorjs/link";
+import Image from "@editorjs/image";
+import Header from "@editorjs/header";
+import CheckList from "@editorjs/checklist";
+import DragDrop from "editorjs-drag-drop";
+import Test from "./Test";
+
+export const EDITOR_JS_TOOLS = {
+  table: Table,
+  list: List,
+  linkTool: LinkTool,
+  image: Image,
+  header: Header,
+  checklist: CheckList,
+  test: Test,
+  drapDrop: DragDrop
+};
